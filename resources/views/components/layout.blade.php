500830e903d8eb213a9448ea862c59f0a236ff52
Abdizzo <abdulkadirismail777@gmail.com>
2023-05-25 12:41:19 +0300

    home-design

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,21 +29,33 @@
     <nav class="flex justify-between items-center mb-4">
         <div class="flex items-center">
 
-            <nav class="bg-white shadow">
-                <div class="container mx-auto px-4">
-                    <div class="flex justify-between items-center py-4">
-                        <a href="/"><img class="w-24" src="{{ asset('images/logo3.png') }}" alt=""
-                                class="logo" /></a>
-                        <div class="flex">
-                            <a class=" font-bold text-gray-800 text-bold hover:text-gray-600 px-4"
-                                href="/">Home</a>
-                            <a class="font-bold text-gray-800 hover:text-gray-600 px-4" href="/employers">Employers</a>
-                            <a class="font-bold text-gray-800 hover:text-gray-600 px-4" href="/job-seekers">Job
-                                Seekers</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
+            
+<nav class="bg-white :bg-slate-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 :border-gray-600">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <a href="https://flowbite.com/" class="flex items-center">
+      <img src="images/worklink1.png" class=" h-12 mr-6" alt="WorkLink Logo">
+      <h1 class="self-center text-2xl font-bold whitespace-nowrap :text-white ">Work<span class = "text-blue-600 font-bold animate-pulse ">Link</span></h1>
+  </a>
+  <div class="flex md:order-2">
+     
+      
+  </div>
+  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white :bg-gray-800 md::bg-gray-900 :border-gray-700">
+      <li>
+        <a class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md::text-blue-500" href="/" >Home</a>
+      </li>
+      <li>
+        <a  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md::hover:text-blue-500 :text-white :hover:bg-gray-700 :hover:text-white md::hover:bg-transparent :border-gray-700" href="/employers">Employers</a>
+      </li>
+      <li>
+        <a class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md::hover:text-blue-500 :text-white :hover:bg-gray-700 :hover:text-white md::hover:bg-transparent :border-gray-700" href="/job-seekers">JobSeekers</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
         </div>
         <ul class="flex space-x-6 mr-6 text-lg">
             @auth
