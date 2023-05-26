ad0f2c581250d644278d58de1884fd16ceb14700
Abdizzo <abdulkadirismail777@gmail.com>
2023-05-26 03:12:33 +0300

    register and login page-design

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,55 +1,80 @@
 <x-layout>
+  <main>
+<style>
+  .btn-animate {
+  animation-name: buttonAnimation;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+
+@keyframes buttonAnimation {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.05);
+  }
+}
+</style>
+ 
+<div class="flex justify-end items-center h-screen">
+  <div class="w-1/2">
+    <!-- Replace the placeholder image URL with your desired image source -->
+    <img class="w-full h-auto max-h-screen" src="images/loginpic3.png" alt="Image">
+  </div>
+  <div class="w-1/2">
+    <div class="mx-4">
+      <div class="bg-gradient-to-r from-purple-400 to-indigo-500 border border-blue-200 p-10 rounded-lg max-w-lg mx-auto mt-24 animate-fadeIn">
+        <header class="text-center">
+          <h2 class="text-4xl font-bold uppercase text-white mb-4">Login</h2>
+          <p class="text-xl text-white mb-6">Login to your account</p>
+        </header>
+
+        <form method="POST" action="/users/authenticate">
+          @csrf
+
+          <div class="mb-6">
+            <label for="email" class="block text-white text-lg mb-2">Email</label>
+            <input value="{{ old('email') }}" type="email"
+              placeholder="Enter your email"
+              class="border border-gray-300 rounded p-2 w-full text-gray-800 bg-white" name="email" />
+            <!-- Error Example -->
+            @error('email')
+            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+            @enderror
+
+          </div>
+
+          <div class="mb-6">
+            <label for="password" class="block text-white text-lg mb-2">Password</label>
+            <input type="password"
+              placeholder="Enter your password"
+              class="border border-gray-300 rounded p-2 w-full text-gray-800 bg-white" name="password" />
+            @error('password')
+            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+            @enderror
+          </div>
+
+          <div class="mb-6">
+            <button type="submit"
+              class="bg-gradient-to-r from-purple-500 to-indigo-600 hover:bg-indigo-700 text-white rounded py-2 px-4 transition-all duration-200 btn-animate">
+              Sign In
+            </button>
+          </div>
+
+          <p class="text-center text-white">
+            Don't have an account?
+            <a href="/register" class="text-blue-200 text-xl hover:underline">Register</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
 
-    <main>
-        <div class="mx-4">
-            <div class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24">
-                <header class="text-center">
-                    <h2 class="text-2xl font-bold uppercase mb-1">
-                        Login
-                    </h2>
-                    <p class="mb-4">Login to your account</p>
-                </header>
 
-                <form method='POST' action="/users/authenticate">
-                    @csrf
-
-                    <div class="mb-6">
-                        <label for="email" class="inline-block text-lg mb-2">Email</label>
-                        <input value="{{ old('email') }}" type="email"
-                            class="border border-gray-200 rounded p-2 w-full" name="email" />
-                        <!-- Error Example -->
-                        @error('email')
-                            <p class='text-red-500 text-xs mt-0.5'>{{ $message }}</p>
-                        @enderror
-
-                    </div>
-
-                    <div class="mb-6">
-                        <label for="password" class="inline-block text-lg mb-2">
-                            Password
-                        </label>
-                        <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password" />
-                        @error('password')
-                            <p class='text-red-500 text-xs mt-0.5'>{{ $message }}</p>
-                        @enderror
-                    </div>
-                    {{-- <div class="mb-6">
-                        <button type="submit" class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
-                            Sign In
-                        </button>
-                    </div> --}}
-
-                    <p>
-                        Dont have an account?
-                        <a href="/register" class="text-laravel">Register</a>
-                    </p>
-            </div>
-            </form>
-        </div>
-        </div>
-    </main>
-
-
-
+  
+  </main>
 </x-layout>
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,98 +1,158 @@
  <x-layout>
-     <div class="container mx-auto sm:px-4">
-         <div class="flex flex-wrap  justify-center">
-             <div class="md:w-2/3 pr-4 pl-4 pt-16 ">
-                 <div
-                     class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300">
-                     <div class="py-3 px-6 mb-0 bg-gray-200 border-b-1 border-gray-300 text-gray-900">
-                         {{ __('Register') }}</div>
-                     <div class="flex-auto p-6">
-                         <form method="POST" action="{{ route('kayit') }}">
-                             @csrf
-                             <div class="mb-4 flex flex-wrap ">
-                                 <label for="name"
-                                     class="md:w-1/3 pr-4 pl-4 pt-2 pb-2 mb-0 leading-normal md:text-right">{{ __('Name') }}</label>
-                                 <div class="md:w-1/2 pr-4 pl-4">
-                                     <input id="name" type="text"
-                                         class="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded @error('name') bg-red-700 @enderror"
-                                         name="name" value="{{ old('name') }}" required autocomplete="name"
-                                         autofocus>
-                                     @error('name')
-                                         <span class="hidden mt-1 text-sm text-red" role="alert">
-                                             <strong>{{ $message }}</strong>
-                                         </span>
-                                     @enderror
-                                 </div>
-                             </div>
-                             <div class="mb-4 flex flex-wrap ">
-                                 <label for="phone"
-                                     class="md:w-1/3 pr-4 pl-4 pt-2 pb-2 mb-0 leading-normal md:text-right">{{ __('Phone Number') }}</label>
-                                 <div class="md:w-1/2 pr-4 pl-4">
-                                     <input id="phone" type="tel"
-                                         class="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded @error('phone') bg-red-700 @enderror"
-                                         name="phone" value="{{ old('phone') }}" required>
-                                     @error('phone')
-                                         <span class="hidden mt-1 text-sm text-red" role="alert">
-                                             <strong>{{ $message }}</strong>
-                                         </span>
-                                     @enderror
-                                 </div>
-                             </div>
-                             <div class="mb-6">
-                                 <label for="email" class="inline-block text-lg mb-2">Email</label>
-                                 <input value="{{ old('email') }}" type="email"
-                                     class="border border-gray-200 rounded p-2 w-full" name="email" />
-                                 <!-- Error Example -->
-                                 @error('email')
-                                     <p class='text-red-500 text-xs mt-0.5'>{{ $message }}</p>
-                                 @enderror
-                             </div>
-                             <div class="mb-6">
-                                 <label for="name" class="inline-block text-lg mb-2">
-                                     Select Your Role
-                                 </label>
-                                 <select name='role' class="border border-gray-200 rounded p-2 w-full">
-                                     <option value="employer" selected="selected">Employer</option>
-                                     <option value="job-seeker">Job-Seeker</option>
-                                 </select>
-                             </div>
-                             <div class="mb-4 flex flex-wrap ">
-                                 <label for="password"
-                                     class="md:w-1/3 pr-4 pl-4 pt-2 pb-2 mb-0 leading-normal md:text-right">{{ __('Password') }}</label>
-                                 <div class="md:w-1/2 pr-4 pl-4">
-                                     <input id="password" type="password"
-                                         class="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded @error('password') bg-red-700 @enderror"
-                                         name="password" required autocomplete="new-password">
-                                     @error('password')
-                                         <span class="hidden mt-1 text-sm text-red" role="alert">
-                                             <strong>{{ $message }}</strong>
-                                         </span>
-                                     @enderror
-                                 </div>
-                             </div>
-                             <div class="mb-4 flex flex-wrap ">
-                                 <label for="password-confirm"
-                                     class="md:w-1/3 pr-4 pl-4 pt-2 pb-2 mb-0 leading-normal md:text-right">{{ __('Confirm Password') }}</label>
-                                 <div class="md:w-1/2 pr-4 pl-4">
-                                     <input id="password-confirm" type="password"
-                                         class="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded"
-                                         name="password_confirmation" required autocomplete="new-password">
-                                 </div>
-                             </div>
-                             <div class="mb-4 flex flex-wrap  mb-0">
-                                 <div class="md:w-1/2 pr-4 pl-4 md:mx-1/3">
-                                     <button type="submit"
-                                         class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600">
-                                         {{ __('Register') }}
-                                     </button>
-                                 </div>
-                             </div>
-                         </form>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
+    
+ <style>
+    .form-container {
+        background: linear-gradient(45deg, #8A4FFF, #3B82F6);
+        color: #fff;
+        padding: 20px;
+        border-radius: 10px;
+        max-width: 400px; /* Adjust the maximum width as needed */
+        margin: 0 auto; /* Center the form horizontally */
+    }
+
+    .form-title {
+        background: rgba(255, 255, 255, 0.1);
+        padding: 10px;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+    }
+
+    .form-field {
+        margin-bottom: 20px;
+    }
+
+    .form-label {
+        display: block;
+        margin-bottom: 5px;
+    }
+
+    .form-input {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid rgba(255, 255, 255, 0.3);
+        border-radius: 5px;
+        background-color: rgba(255, 255, 255, 0.1);
+        color: #fff;
+    }
+
+    .form-input::placeholder {
+        color: rgba(255, 255, 255, 0.5);
+    }
+
+    .form-input:focus {
+        outline: none;
+        border-color: rgba(255, 255, 255, 0.5);
+    }
+
+    .form-select {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid rgba(255, 255, 255, 0.3);
+        border-radius: 5px;
+        background-color: rgba(255, 255, 255, 0.1);
+        color: #fff;
+    }
+
+    .form-button {
+        background: linear-gradient(45deg, #8A4FFF, #3B82F6);
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background 0.3s ease;
+    }
+
+    .form-button:hover {
+        background: linear-gradient(45deg, #3B82F6, #8A4FFF);
+    }
+</style>
+
+
+
+<div class="container mx-auto sm:px-4">
+    <div class="flex flex-wrap justify-center">
+        <div class="md:w-2/3 pr-4 pl-4 pt-16">
+            <div class="relative flex flex-col min-w-0 rounded break-words form-container">
+                <div class="py-3 px-6 mb-0 form-title">
+                    {{ __('Register') }}
+                </div>
+                <div class="flex-auto p-6">
+                    <form method="POST" action="{{ route('kayit') }}">
+                        @csrf
+
+                        <div class="form-field">
+                            <label for="name" class="form-label">{{ __('Name') }}</label>
+                            <input id="name" type="text" class="form-input" name="name" value="{{ old('name') }}"
+                                placeholder="{{ __('Enter your name') }}" required autocomplete="name" autofocus>
+                            @error('name')
+                            <span class="text-sm text-red" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-field">
+                            <label for="phone" class="form-label">{{ __('Phone Number') }}</label>
+                            <input id="phone" type="tel" class="form-input" name="phone" value="{{ old('phone') }}"
+                                placeholder="{{ __('Enter your phone number') }}" required>
+                            @error('phone')
+                            <span class="text-sm text-red" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-field">
+                            <label for="email" class="form-label">Email</label>
+                            <input value="{{ old('email') }}" type="email" class="form-input" name="email"
+                                placeholder="{{ __('Enter your email') }}" />
+                            @error('email')
+                            <p class='text-red-500 text-xs mt-0.5'>{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="form-field">
+                            <label for="name" class="form-label">Select Your Role</label>
+                            <select name='role' class="form-select">
+                                <option value="employer" selected="selected">Employer</option>
+                                <option value="job-seeker">Job-Seeker</option>
+                            </select>
+                        </div>
+
+                        <div class="form-field">
+                            <label for="password" class="form-label">{{ __('Password') }}</label>
+                            <input id="password" type="password" class="form-input" name="password" required
+                                placeholder="{{ __('Enter your password') }}" autocomplete="new-password">
+                            @error('password')
+                            <span class="text-sm text-red" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-field">
+                            <label for="password-confirm" class="form-label">{{ __('Confirm Password') }}</label>
+                            <input id="password-confirm" type="password" class="form-input"
+                                name="password_confirmation" required placeholder="{{ __('Confirm your password') }}"
+                                autocomplete="new-password">
+                        </div>
+
+                        <div class="form-field mb-0">
+                            <button type="submit" class="form-button">
+                                <span class="animate-pulse">{{ __('Register') }}</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
      {{-- <main>
          <div class="mx-4">
              <div class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24">
